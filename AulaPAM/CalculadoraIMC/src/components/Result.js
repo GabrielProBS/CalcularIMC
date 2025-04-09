5ce143c7672ebd762752efafe3380a4e695d8b8c
GabrielProBS <gabriel.silva3804@etec.sp.gov.br>
2025-04-09 10:09:30 -0300

    Update Result.js

--- AulaPAM/CalculadoraIMC/src/components/Result.js
+++ AulaPAM/CalculadoraIMC/src/components/Result.js
@@ -1,19 +1,20 @@
 import React from "react";
-import {Text, StyleSheet} from 'react-native';
+import { Text, StyleSheet } from "react-native";
 
-const Result = ({imc}) => {
+const Result = ({imc})=>{
     return (
-        <Text style={styles.result}>Seu IMC é: {imc}</Text>
-    );
-};
+        <Text style={styles.result}>Seu IMC é: {imc} </Text>
+    )
+}
 
 const styles = StyleSheet.create({
     result: {
         marginTop: 20,
         fontSize: 24,
         textAlign: 'center',
-        color: '#333',
-    },
-});
+        color: '#0D0D0D',
+        fontFamily: 'monospace',
+    }
+})
 
 export default Result;
