447c69704101d4ab53d66cfd40f2779e0485507b
GabrielProBS <gabriel.silva3804@etec.sp.gov.br>
2025-04-09 10:10:13 -0300

    Update App.js

--- AulaPAM/CalculadoraIMC/App.js
+++ AulaPAM/CalculadoraIMC/App.js
@@ -1,23 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, View } from "react-native";
+import Title from "./src/components/Title";
+import FormIMC from "./src/components/FormIMC";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Gabriel pro</Text>
-      <Text>Caguei nas calças</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+    return (
+        <View style={styles.container}>
+            <Title/>
+            <FormIMC/>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F2C894',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-});
+const styles= StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 250,
+        backgroundColor: '#9C9FA6',
+    },
+})
